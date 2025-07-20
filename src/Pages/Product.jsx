00678a86ec744569003cac03eec3adaf5a6320da
khalid muzemil <khalidmuzemil12@gmail.com>
2025-07-21 02:16:18 +0300

    add new components and styling

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -2,6 +2,9 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom'
 import Breadcrum from '../components/Breadcrums/Breadcrum'
+import ProductDisplay from '../components/ProductDisplay/ProductDisplay'
+import DescriptionBox from '../components/DescriptionBox/DescriptionBox'
+import RelatedProducts from '../components/RelatedProducts/RelatedProducts'
 
 const Product = () => {
   const {all_product} = useContext(ShopContext)
@@ -10,6 +13,9 @@ const Product = () => {
   return (
     <div>
       <Breadcrum product={product} />
+      <ProductDisplay product={product} />
+      <DescriptionBox />
+      <RelatedProducts />
     </div>
   )
 }
---
+++ src/components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, possimus! Consequatur temporibus laborum sunt consectetur praesentium expedita accusamus quisquam! Reiciendis deserunt dolorem, tenetur neque architecto voluptates velit molestiae saepe at?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta quidem suscipit atque ratione cumque ex non ipsam harum maiores itaque alias eos earum a quibusdam doloremque, qui omnis unde voluptates?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptatem adipisci consequatur! Nihil similique voluptatibus labore, nam eum alias. Ad corporis similique vel quaerat ex a atque ab corrupti voluptas?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, possimus! Consequatur temporibus laborum sunt consectetur praesentium expedita accusamus quisquam! Reiciendis deserunt dolorem, tenetur neque architecto voluptates velit molestiae saepe at?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta quidem suscipit atque ratione cumque ex non ipsam harum maiores itaque alias eos earum a quibusdam doloremque, qui omnis unde voluptates?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptatem adipisci consequatur! Nihil similique voluptatibus labore, nam eum alias. Ad corporis similique vel quaerat ex a atque ab corrupti voluptas? 
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
