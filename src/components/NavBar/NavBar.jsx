65b3341859551ce95038cc9666d6762759b6b720
khalid muzemil <khalidmuzemil12@gmail.com>
2025-07-23 08:51:57 +0300

    final touch of the react frontend

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,17 @@
-import React, {useState} from 'react'
+import React, {useState , useContext} from 'react'
 import './NavBar.css'
 
 import logo from '../Assets/logo.png'; // Adjust the path as necessary
 import cart_icon from '../Assets/cart_icon.png'; // Adjust the path as necessary
 import { Link } from 'react-router-dom';
+import { ShopContext } from '../../Context/ShopContext';
 
 
 
 const NavBar = () => {
 
     const [menu,setMenu] = useState("shop");
+    const {getTotalCartItems} = useContext(ShopContext)
     
   return (
     <div className='navbar'>
@@ -26,7 +28,7 @@ const NavBar = () => {
         <div className="nav-login-cart">
             <Link to="/login" style={{textDecoration: 'none'}}><button>Login</button></Link>
             <Link to="/cart" style={{textDecoration: 'none'}}><img src={cart_icon} alt="" /></Link>
-            <div className="nav-cart-count">0</div>
+            <div className="nav-cart-count">{getTotalCartItems()}</div>
         </div>
     </div>
               )
